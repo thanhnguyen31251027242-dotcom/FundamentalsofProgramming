5dd10efbc493ad151bb5723577749b9716786ec4
thanhnguyen31251027242-dotcom <thanhnguyen.31251027242@st.ueh.edu.vn>
2026-05-25 15:24:36 +0700

    bai tap 7

--- FundamentalsofProgramming/Session_07/Ex_07.cs
+++ FundamentalsofProgramming/Session_07/Ex_07.cs
@@ -6,69 +6,35 @@ namespace FundamentalsofProgramming.Session_07
 {
     internal class Ex_07
     {
-        //Create an integer matrix N x M (N,M was prompted from user) randomly.
-        static int[,] Taomang_com(int rows, int columns)
+        static void NhapMang(int[] arr, int n)
         {
-            int[,] a = new int[rows, columns];
-            for(int i=0; i<rows; i++)
-            {
+            string[] input = Console.ReadLine().Split(' ');
 
-                for (int j = 0; j < columns; j++)
-                {
-                    Console.Write($"Nhap phan tu a[{i},{j}] = ");
-                    a[i, j] = int.Parse(Console.ReadLine());
-                }
-            }
-            return a;
-        }
-        static int[,] Taomang_random(int rows, int columns)
-        {
-            int[,] a = new int[rows, columns];
-            Random random = new Random();
-            for(int i=0; i<rows; i++)
+            for (int i = 0; i < n; i++)
             {
-                for(int j=0; j<columns;j++)
-                {
-                    a[i, j] = random.Next(0, 100);
-                }
+                arr[i] = int.Parse(input[i]);
             }
-            return a;
         }
-        //In mang
-        static void Inmang(int[,]a)
+
+        // Hàm xuất mảng
+        static void XuatMang(int[] arr, int n)
         {
-            for(int i=0;i< a.GetLength(0);i++)
+            for (int i = 0; i < n; i++)
             {
-                for(int j=0;j<a.GetLength(1);j++)
-                {
-                    Console.Write($"{a[i,j]}\t");
-                }
-                Console.WriteLine();
+                Console.Write(arr[i] + " ");
             }
         }
-        //in 1 dong/cot
-        static void Indong(int[,]a, int rowIndex)
+
+        // Main
+        static void Main()
         {
-            for(int j=0; j<a.GetLength(1);j++)
-            {
-                Console.Write($"{a[rowIndex,j]}\t");
-            }
-        }
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Nhap so dong: "); int rows = int.Parse(Console.ReadLine());
-            Console.WriteLine("Nhap so cot: "); int columns = int.Parse(Console.ReadLine());
-            ////tao mang bang com
-            int[,] a = Taomang_com(rows, columns);
-            Inmang(a);
-            //tao mang ngau nhien
-            int[,]a= Taomang_random(rows,columns);
-            Console.WriteLine("Mang sau khi nhap");
-            Inmang(a);
-            //in 1 dong/cot
-            Console.WriteLine("Nhap dong can in: ");
-            int rowIndex = int.Parse(Console.ReadLine());
-            Indong(a,rowIndex);
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            NhapMang(arr, n);
+
+            XuatMang(arr, n);
         }
     }
 }
